be49371453f361ba45ada9b4e05bcc25ddd1fe90
Yulia <yulia1201556@gmail.com>
2022-08-18 20:17:11 +0300

    Выводмассива на экран

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2.выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+Console.Clear();
+
+int[] arr = { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+
+    Console.WriteLine();
+}
+PrintArray(arr);
